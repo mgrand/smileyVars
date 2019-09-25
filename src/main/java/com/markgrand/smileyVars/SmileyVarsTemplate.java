f857a356e4230cf77cfa61978092ed710dfc5a91
Mark Grand <mgrand@markgrand.com>
2019-09-24 21:27:53 -0400

    New File.

---
+++ src/main/java/com/markgrand/smileyVars/SmileyVarsTemplate.java
@@ -0,0 +1,20 @@
+package com.markgrand.smileyVars;
+
+/**
+ * A template for SQL that expands to leave out extraneous portions of a query.
+ *
+ * @author Mark Grand
+ */
+public class SmileyVarsTemplate {
+    private final String sql;
+
+
+    /**
+     * Constructor
+     *
+     * @param sql The template body.
+     */
+    public SmileyVarsTemplate(String sql) {
+        this.sql = sql;
+    }
+}
