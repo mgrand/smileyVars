0312ecedf104d31b3237f9ce136925308277c54c
Mark Grand <mgrand@markgrand.com>
2019-09-26 18:24:33 -0400

    methods to instantiate templates

--- src/main/java/com/markgrand/smileyVars/SmileyVarsTemplate.java
+++ src/main/java/com/markgrand/smileyVars/SmileyVarsTemplate.java
@@ -1,19 +1,76 @@
 package com.markgrand.smileyVars;
 
+import java.util.Map;
+
 /**
  * A template for SQL that expands to leave out extraneous portions of a query.
  *
  * @author Mark Grand
  */
 public class SmileyVarsTemplate {
+    private final Tokenizer.TokenizerBuilder builder;
     private final String sql;
 
     /**
      * Constructor
+     */
+    private SmileyVarsTemplate(String sql, Tokenizer.TokenizerBuilder builder) {
+        this.builder = builder;
+        this.sql = sql;
+    }
+
+    /**
+     * Create a template for ANSI-compliant SQL
      *
      * @param sql The template body.
+     * @return the template.
      */
-    public SmileyVarsTemplate(String sql) {
-        this.sql = sql;
+    public static SmileyVarsTemplate ansiTemplate(String sql) {
+        return new SmileyVarsTemplate(sql, Tokenizer.builder().configureForAnsi());
+    }
+
+    /**
+     * Create a template for PostgreSQL SQL
+     *
+     * @param sql The template body.
+     * @return the template.
+     */
+    public static SmileyVarsTemplate postgresqlTemplate(String sql) {
+        return new SmileyVarsTemplate(sql, Tokenizer.builder().configureForPostgresql());
+    }
+
+    /**
+     * Create a template for Oracle SQL
+     *
+     * @param sql The template body.
+     * @return the template.
+     */
+    public static SmileyVarsTemplate oracleTemplate(String sql) {
+        return new SmileyVarsTemplate(sql, Tokenizer.builder().configureForOracle());
+    }
+
+    /**
+     * Create a template for Transact SQL (SQL Server)
+     *
+     * @param sql The template body.
+     * @return the template.
+     */
+    public static SmileyVarsTemplate sqlServerTemplate(String sql) {
+        return new SmileyVarsTemplate(sql, Tokenizer.builder().configureForSqlServer());
+    }
+
+    /**
+     * Apply the values in the given Map to this template.
+     *
+     * @param values Apply the given values to this template
+     * @return the template
+     */
+    public String apply(Map<String, String> values) {
+        Tokenizer tokenizer = builder.build(sql);
+        StringBuilder sb = new StringBuilder(sql.length()*2);
+
+        while (tokenizer.hasNext()) {
+            
+        }
     }
 }
