0f09833942865cc0e6711d2dffdde8113d8db9af
Mark Grand <mgrand@markgrand.com>
2019-09-29 13:28:47 -0400

    ValueFormatters now have names

--- src/main/java/com/markgrand/smileyVars/ValueFormatter.java
+++ src/main/java/com/markgrand/smileyVars/ValueFormatter.java
@@ -4,23 +4,26 @@ import java.util.function.Function;
 import java.util.function.Predicate;
 
 /**
- * Format objects that are values of smileyVars as SQL literals. If you want SmileyVars to format objects
- * that are instance of a class that is not a number, string or date.
+ * Format objects that are values of smileyVars as SQL literals. If you want SmileyVars to format objects that are
+ * instance of a class that is not a number, string or date.
  */
 class ValueFormatter {
     private final Predicate<Object> appliesTo;
     private final Function<Object, String> formattingFunction;
+    private final String description;
 
     /**
      * Constructor
      *
-     * @param predicate This should return true if, and only if, its argument is something that the formattingFunction can format.
+     * @param predicate          This should return true if, and only if, its argument is something that the
+     *                           formattingFunction can format.
      * @param formattingFunction If given a value that the predicate returns true for, this should return a string that
      *                           represents to given value as an SQL literal.
      */
-    ValueFormatter(Predicate<Object> predicate, Function<Object, String> formattingFunction) {
+    ValueFormatter(Predicate<Object> predicate, Function<Object, String> formattingFunction, String description) {
         appliesTo = predicate;
         this.formattingFunction = formattingFunction;
+        this.description = description;
     }
 
     /**
@@ -33,7 +36,24 @@ class ValueFormatter {
         return appliesTo.test(value);
     }
 
+    /**
+     * Format the given value as an SQL literal.
+     * <p><b>Note:</b> If {@link #isApplicable(Object)} returns false for this value, then the result of calling this
+     * method is undefined.</p>
+     *
+     * @param value The value to be formatted asn an SQL literal
+     * @return the formatted string.
+     */
     String format(Object value) {
         return formattingFunction.apply(value);
+    }
+
+    /**
+     * Get the description of this formatter.
+     *
+     * @return the description.
+     */
+    String getDescription() {
+        return description;
     }
 }
