3a00b965dd3a425085f0645a0afc1032c09dea8a
Mark Grand <mgrand@markgrand.com>
2019-10-19 21:05:49 -0400

    Tokenizer recognizes vars in unbracketed SQL. nested brackets throw an exception.

---
+++ src/main/java/com/markgrand/smileyVars/UnsupportedFeatureException.java
@@ -0,0 +1,20 @@
+package com.markgrand.smileyVars;
+
+/**
+ * This is thrown to indicate that SmileyVars feature is not yet supported.
+ *
+ * @see ValueFormatterRegistry
+ */
+@SuppressWarnings("WeakerAccess")
+public class UnsupportedFeatureException extends SmileyVarsException {
+    /**
+     * Constructs a new exception with the specified detail message. The cause is not initialized, and may subsequently
+     * be initialized by a call to {@link #initCause}.
+     *
+     * @param message the detail message. The detail message is saved for later retrieval by the {@link #getMessage()}
+     *                method.
+     */
+    UnsupportedFeatureException(String message) {
+        super(message);
+    }
+}
