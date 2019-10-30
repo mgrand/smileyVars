e996cec036113f3c3d6cc2f9f2d8d7c119422567
Mark Grand <mgrand@markgrand.com>
2019-10-30 19:57:24 -0400

    DatabaseType passes unit test

--- src/test/java/com/markgrand/smileyVars/DatabaseTypeTest.java
+++ src/test/java/com/markgrand/smileyVars/DatabaseTypeTest.java
@@ -1,10 +1,13 @@
 package com.markgrand.smileyVars;
 
+import mockit.Capturing;
 import mockit.Expectations;
 import mockit.Mocked;
+import mockit.Verifications;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
 
-import java.sql.DatabaseMetaData;
+import java.sql.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -44,6 +47,149 @@ class DatabaseTypeTest {
             metaData.getDatabaseProductName(); result = "Apache Derby";
         }};
         assertEquals(DatabaseType.ANSI, DatabaseType.inferDatabaseType(metaData));
+    }
+
+    @Test
+    void inferDatabaseTypeEnterprise(@Mocked DatabaseMetaData metaData) throws Exception {
+        new Expectations() {{
+            metaData.getDatabaseProductName(); result = "EnterpriseDB";
+        }};
+        assertEquals(DatabaseType.POSTGRESQL, DatabaseType.inferDatabaseType(metaData));
+    }
+
+    @Test
+    void inferDatabaseTypeFirebird(@Mocked DatabaseMetaData metaData) throws Exception {
+        new Expectations() {{
+            metaData.getDatabaseProductName(); result = "Firebird";
+        }};
+        assertEquals(DatabaseType.ANSI, DatabaseType.inferDatabaseType(metaData));
+    }
+
+    @Test
+    void inferDatabaseTypeH2(@Mocked DatabaseMetaData metaData) throws Exception {
+        new Expectations() {{
+            metaData.getDatabaseProductName(); result = "H2";
+        }};
+        assertEquals(DatabaseType.ANSI, DatabaseType.inferDatabaseType(metaData));
+    }
+
+    @Test
+    void inferDatabaseTypeHdb(@Mocked DatabaseMetaData metaData) throws Exception {
+        new Expectations() {{
+            metaData.getDatabaseProductName(); result = "HDB";
+        }};
+        assertEquals(DatabaseType.ANSI, DatabaseType.inferDatabaseType(metaData));
+    }
+
+    @Test
+    void inferDatabaseTypeHsql(@Mocked DatabaseMetaData metaData) throws Exception {
+        new Expectations() {{
+            metaData.getDatabaseProductName(); result = "HSQL Database Engine";
+        }};
+        assertEquals(DatabaseType.ANSI, DatabaseType.inferDatabaseType(metaData));
+    }
+
+    @Test
+    void inferDatabaseTypeInformix(@Mocked DatabaseMetaData metaData) throws Exception {
+        new Expectations() {{
+            metaData.getDatabaseProductName(); result = "Informix Dynamic Server";
+        }};
+        assertEquals(DatabaseType.ANSI, DatabaseType.inferDatabaseType(metaData));
+    }
+
+    @Test
+    void inferDatabaseTypeIngres(@Mocked DatabaseMetaData metaData) throws Exception {
+        new Expectations() {{
+            metaData.getDatabaseProductName(); result = "ingres";
+        }};
+        assertEquals(DatabaseType.ANSI, DatabaseType.inferDatabaseType(metaData));
+    }
+
+    @Test
+    void inferDatabaseTypeMaria(@Mocked DatabaseMetaData metaData) throws Exception {
+        new Expectations() {{
+            metaData.getDatabaseProductName(); result = "MariaDB";
+            metaData.getDriverName(); result = "MariaDB";
+        }};
+        assertEquals(DatabaseType.ANSI, DatabaseType.inferDatabaseType(metaData));
+    }
+
+    @Test
+    void inferDatabaseTypeMySql(@Mocked DatabaseMetaData metaData) throws Exception {
+        new Expectations() {{
+            metaData.getDatabaseProductName(); result = "MySQL";
+        }};
+        assertEquals(DatabaseType.ANSI, DatabaseType.inferDatabaseType(metaData));
+    }
+
+    @Test
+    void inferDatabaseTypeOracle(@Mocked DatabaseMetaData metaData) throws Exception {
+        new Expectations() {{
+            metaData.getDatabaseProductName(); result = "Oracle";
+        }};
+        assertEquals(DatabaseType.ORACLE, DatabaseType.inferDatabaseType(metaData));
+    }
+
+    @Test
+    void inferDatabaseTypePostgreSQL(@Mocked DatabaseMetaData metaData) throws Exception {
+        new Expectations() {{
+            metaData.getDatabaseProductName(); result = "PostgreSQL";
+        }};
+        assertEquals(DatabaseType.POSTGRESQL, DatabaseType.inferDatabaseType(metaData));
+    }
+
+    @Test
+    void inferDatabaseTypeSqlServer(@Mocked DatabaseMetaData metaData) throws Exception {
+        new Expectations() {{
+            metaData.getDatabaseProductName(); result = "Microsoft SQL Server";
+        }};
+        assertEquals(DatabaseType.SQL_SERVER, DatabaseType.inferDatabaseType(metaData));
+    }
+
+    @Test
+    void inferDatabaseTypeSybase(@Mocked DatabaseMetaData metaData) throws Exception {
+        new Expectations() {{
+            metaData.getDatabaseProductName(); result = "Sybase SQL Server";
+        }};
+        assertEquals(DatabaseType.ANSI, DatabaseType.inferDatabaseType(metaData));
+    }
+
+    @Test
+    void inferDatabaseTypeAdaptive(@Mocked DatabaseMetaData metaData) throws Exception {
+        new Expectations() {{
+            metaData.getDatabaseProductName(); result = "Adaptive Server Enterprise";
+        }};
+        assertEquals(DatabaseType.ANSI, DatabaseType.inferDatabaseType(metaData));
+    }
+
+    @Test
+    void inferDatabaseTypeAAnywhere(@Mocked DatabaseMetaData metaData) throws Exception {
+        new Expectations() {{
+            metaData.getDatabaseProductName(); result = "Adaptive Server Anywhere";
+        }};
+        assertEquals(DatabaseType.ANSI, DatabaseType.inferDatabaseType(metaData));
+    }
+
+    @Test
+    void inferDatabaseTypeSAnywhere(@Mocked DatabaseMetaData metaData, @Capturing Logger logger) throws Exception {
+        new Expectations() {{
+            metaData.getDatabaseProductName(); result = "SQL Anywhere";
+        }};
+        assertEquals(DatabaseType.ANSI, DatabaseType.inferDatabaseType(metaData));
+        new Verifications() {{
+            logger.warn(anyString); maxTimes = 0;
+        }};
+    }
+
+    @Test
+    void inferDatabaseTypeUnknown(@Mocked DatabaseMetaData metaData, @Capturing Logger logger) throws Exception {
+        new Expectations() {{
+            metaData.getDatabaseProductName(); result = "xxxxxxxx";
+        }};
+        assertEquals(DatabaseType.ANSI, DatabaseType.inferDatabaseType(metaData));
+        new Verifications() {{
+            logger.warn(anyString); times = 1;
+        }};
     }
 
     @Test
