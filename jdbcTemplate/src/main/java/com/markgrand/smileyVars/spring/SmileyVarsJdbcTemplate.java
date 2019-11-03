1643d98b5ecd5e1cb024a81537aab28d040b3a53
Mark Grand <mgrand@markgrand.com>
2019-11-03 13:42:07 -0500

    Update SmileyVarsJdbcTemplate.java
    Created overridden methods.

--- jdbcTemplate/src/main/java/com/markgrand/smileyVars/spring/SmileyVarsJdbcTemplate.java
+++ jdbcTemplate/src/main/java/com/markgrand/smileyVars/spring/SmileyVarsJdbcTemplate.java
@@ -2,6 +2,8 @@ package com.markgrand.smileyVars.spring;
 
 import com.markgrand.smileyVars.DatabaseType;
 import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.SQLWarningException;
 import org.springframework.jdbc.core.*;
@@ -22,7 +24,9 @@ import java.util.*;
  */
 @SuppressWarnings("OptionalUsedAsFieldOrParameterType")
 public class SmileyVarsJdbcTemplate extends JdbcTemplate {
-    Optional<DatabaseType> dbType = Optional.empty();
+    private static Logger logger = LoggerFactory.getLogger(SmileyVarsJdbcTemplate.class);
+
+    private Optional<DatabaseType> dbType = Optional.empty();
 
     public SmileyVarsJdbcTemplate() {
         super();
@@ -30,100 +34,20 @@ public class SmileyVarsJdbcTemplate extends JdbcTemplate {
 
     public SmileyVarsJdbcTemplate(@NotNull DataSource dataSource) {
         super(dataSource);
+        inferDbType(dataSource);
+    }
+
+    private void inferDbType(@NotNull DataSource dataSource) {
         try (Connection conn = dataSource.getConnection()) {
             dbType = Optional.of(DatabaseType.inferDatabaseType(conn.getMetaData()));
         } catch (SQLException e) {
-            e.printStackTrace();
+            logger.debug("Error inferring database type from connection", e);
         }
     }
 
     public SmileyVarsJdbcTemplate(DataSource dataSource, boolean lazyInit) {
         super(dataSource, lazyInit);
-    }
-
-    @Override
-    public void setIgnoreWarnings(boolean ignoreWarnings) {
-        super.setIgnoreWarnings(ignoreWarnings);
-    }
-
-    @Override
-    public boolean isIgnoreWarnings() {
-        return super.isIgnoreWarnings();
-    }
-
-    @Override
-    public void setFetchSize(int fetchSize) {
-        super.setFetchSize(fetchSize);
-    }
-
-    @Override
-    public int getFetchSize() {
-        return super.getFetchSize();
-    }
-
-    @Override
-    public void setMaxRows(int maxRows) {
-        super.setMaxRows(maxRows);
-    }
-
-    @Override
-    public int getMaxRows() {
-        return super.getMaxRows();
-    }
-
-    @Override
-    public void setQueryTimeout(int queryTimeout) {
-        super.setQueryTimeout(queryTimeout);
-    }
-
-    @Override
-    public int getQueryTimeout() {
-        return super.getQueryTimeout();
-    }
-
-    @Override
-    public void setSkipResultsProcessing(boolean skipResultsProcessing) {
-        super.setSkipResultsProcessing(skipResultsProcessing);
-    }
-
-    @Override
-    public boolean isSkipResultsProcessing() {
-        return super.isSkipResultsProcessing();
-    }
-
-    @Override
-    public void setSkipUndeclaredResults(boolean skipUndeclaredResults) {
-        super.setSkipUndeclaredResults(skipUndeclaredResults);
-    }
-
-    @Override
-    public boolean isSkipUndeclaredResults() {
-        return super.isSkipUndeclaredResults();
-    }
-
-    @Override
-    public void setResultsMapCaseInsensitive(boolean resultsMapCaseInsensitive) {
-        super.setResultsMapCaseInsensitive(resultsMapCaseInsensitive);
-    }
-
-    @Override
-    public boolean isResultsMapCaseInsensitive() {
-        return super.isResultsMapCaseInsensitive();
-    }
-
-    @Override
-    public <T> T execute(@NotNull ConnectionCallback<T> action) throws DataAccessException {
-        return super.execute(action);
-    }
-
-    @Override
-    protected Connection createConnectionProxy(Connection con) {
-        return super.createConnectionProxy(con);
-    }
-
-    @Override
-    public <T> T execute(@NotNull StatementCallback<T> action) throws DataAccessException {
-        return super.execute(action);
+        inferDbType(dataSource);
     }
 
     @Override
@@ -427,31 +351,6 @@ public class SmileyVarsJdbcTemplate extends JdbcTemplate {
     }
 
     @Override
-    protected Map<String, Object> processResultSet(ResultSet rs, ResultSetSupportingSqlParameter param) throws SQLException {
-        return super.processResultSet(rs, param);
-    }
-
-    @Override
-    protected RowMapper<Map<String, Object>> getColumnMapRowMapper() {
-        return super.getColumnMapRowMapper();
-    }
-
-    @Override
-    protected <T> RowMapper<T> getSingleColumnRowMapper(Class<T> requiredType) {
-        return super.getSingleColumnRowMapper(requiredType);
-    }
-
-    @Override
-    protected Map<String, Object> createResultsMap() {
-        return super.createResultsMap();
-    }
-
-    @Override
-    protected void applyStatementSettings(@NotNull Statement stmt) throws SQLException {
-        super.applyStatementSettings(stmt);
-    }
-
-    @Override
     protected PreparedStatementSetter newArgPreparedStatementSetter(Object[] args) {
         return super.newArgPreparedStatementSetter(args);
     }
@@ -462,33 +361,9 @@ public class SmileyVarsJdbcTemplate extends JdbcTemplate {
     }
 
     @Override
-    protected void handleWarnings(Statement stmt) throws SQLException {
-        super.handleWarnings(stmt);
-    }
-
-    @Override
-    protected void handleWarnings(SQLWarning warning) throws SQLWarningException {
-        super.handleWarnings(warning);
-    }
-
-    @Override
-    protected DataAccessException translateException(String task, String sql, SQLException ex) {
-        return super.translateException(task, sql, ex);
-    }
-
-    @Override
     public void setDataSource(DataSource dataSource) {
         super.setDataSource(dataSource);
-    }
-
-    @Override
-    public DataSource getDataSource() {
-        return super.getDataSource();
-    }
-
-    @Override
-    protected DataSource obtainDataSource() {
-        return super.obtainDataSource();
+        inferDbType(dataSource);
     }
 
     @Override
@@ -496,60 +371,9 @@ public class SmileyVarsJdbcTemplate extends JdbcTemplate {
         super.setDatabaseProductName(dbName);
     }
 
-    @Override
-    public void setExceptionTranslator(SQLExceptionTranslator exceptionTranslator) {
-        super.setExceptionTranslator(exceptionTranslator);
-    }
-
-    @Override
-    public SQLExceptionTranslator getExceptionTranslator() {
-        return super.getExceptionTranslator();
-    }
-
-    @Override
-    public void setLazyInit(boolean lazyInit) {
-        super.setLazyInit(lazyInit);
-    }
-
-    @Override
-    public boolean isLazyInit() {
-        return super.isLazyInit();
-    }
-
-    @Override
-    public void afterPropertiesSet() {
-        super.afterPropertiesSet();
-    }
-
     /**
      * Returns a hash code value for the object. This method is supported for the benefit of hash tables such as those
      * provided by {@link HashMap}.
-     * <p>
-     * The general contract of {@code hashCode} is:
-     * <ul>
-     * <li>Whenever it is invoked on the same object more than once during
-     *     an execution of a Java application, the {@code hashCode} method
-     *     must consistently return the same integer, provided no information
-     *     used in {@code equals} comparisons on the object is modified.
-     *     This integer need not remain consistent from one execution of an
-     *     application to another execution of the same application.
-     * <li>If two objects are equal according to the {@code equals(Object)}
-     *     method, then calling the {@code hashCode} method on each of
-     *     the two objects must produce the same integer result.
-     * <li>It is <em>not</em> required that if two objects are unequal
-     *     according to the {@link Object#equals(Object)}
-     *     method, then calling the {@code hashCode} method on each of the
-     *     two objects must produce distinct integer results.  However, the
-     *     programmer should be aware that producing distinct integer results
-     *     for unequal objects may improve the performance of hash tables.
-     * </ul>
-     * <p>
-     * As much as is reasonably practical, the hashCode method defined by
-     * class {@code Object} does return distinct integers for distinct
-     * objects. (This is typically implemented by converting the internal
-     * address of the object into an integer, but this implementation
-     * technique is not required by the
-     * Java&trade; programming language.)
      *
      * @return a hash code value for this object.
      * @see Object#equals(Object)
@@ -562,42 +386,6 @@ public class SmileyVarsJdbcTemplate extends JdbcTemplate {
 
     /**
      * Indicates whether some other object is "equal to" this one.
-     * <p>
-     * The {@code equals} method implements an equivalence relation on non-null object references:
-     * <ul>
-     * <li>It is <i>reflexive</i>: for any non-null reference value
-     *     {@code x}, {@code x.equals(x)} should return
-     *     {@code true}.
-     * <li>It is <i>symmetric</i>: for any non-null reference values
-     *     {@code x} and {@code y}, {@code x.equals(y)}
-     *     should return {@code true} if and only if
-     *     {@code y.equals(x)} returns {@code true}.
-     * <li>It is <i>transitive</i>: for any non-null reference values
-     *     {@code x}, {@code y}, and {@code z}, if
-     *     {@code x.equals(y)} returns {@code true} and
-     *     {@code y.equals(z)} returns {@code true}, then
-     *     {@code x.equals(z)} should return {@code true}.
-     * <li>It is <i>consistent</i>: for any non-null reference values
-     *     {@code x} and {@code y}, multiple invocations of
-     *     {@code x.equals(y)} consistently return {@code true}
-     *     or consistently return {@code false}, provided no
-     *     information used in {@code equals} comparisons on the
-     *     objects is modified.
-     * <li>For any non-null reference value {@code x},
-     *     {@code x.equals(null)} should return {@code false}.
-     * </ul>
-     * <p>
-     * The {@code equals} method for class {@code Object} implements
-     * the most discriminating possible equivalence relation on objects;
-     * that is, for any non-null reference values {@code x} and
-     * {@code y}, this method returns {@code true} if and only
-     * if {@code x} and {@code y} refer to the same object
-     * ({@code x == y} has the value {@code true}).
-     * <p>
-     * Note that it is generally necessary to override the {@code hashCode}
-     * method whenever this method is overridden, so as to maintain the
-     * general contract for the {@code hashCode} method, which states
-     * that equal objects must have equal hash codes.
      *
      * @param obj the reference object with which to compare.
      * @return {@code true} if this object is the same as the obj argument; {@code false} otherwise.
@@ -607,56 +395,6 @@ public class SmileyVarsJdbcTemplate extends JdbcTemplate {
     @Override
     public boolean equals(Object obj) {
         return super.equals(obj);
-    }
-
-    /**
-     * Creates and returns a copy of this object.  The precise meaning of "copy" may depend on the class of the object.
-     * The general intent is that, for any object {@code x}, the expression:
-     * <blockquote>
-     * <pre>
-     * x.clone() != x</pre></blockquote>
-     * will be true, and that the expression:
-     * <blockquote>
-     * <pre>
-     * x.clone().getClass() == x.getClass()</pre></blockquote>
-     * will be {@code true}, but these are not absolute requirements. While it is typically the case that:
-     * <blockquote>
-     * <pre>
-     * x.clone().equals(x)</pre></blockquote>
-     * will be {@code true}, this is not an absolute requirement.
-     * <p>
-     * By convention, the returned object should be obtained by calling {@code super.clone}.  If a class and all of its
-     * superclasses (except {@code Object}) obey this convention, it will be the case that {@code x.clone().getClass()
-     * == x.getClass()}.
-     * <p>
-     * By convention, the object returned by this method should be independent of this object (which is being cloned).
-     * To achieve this independence, it may be necessary to modify one or more fields of the object returned by {@code
-     * super.clone} before returning it.  Typically, this means copying any mutable objects that comprise the internal
-     * "deep structure" of the object being cloned and replacing the references to these objects with references to the
-     * copies.  If a class contains only primitive fields or references to immutable objects, then it is usually the
-     * case that no fields in the object returned by {@code super.clone} need to be modified.
-     * <p>
-     * The method {@code clone} for class {@code Object} performs a specific cloning operation. First, if the class of
-     * this object does not implement the interface {@code Cloneable}, then a {@code CloneNotSupportedException} is
-     * thrown. Note that all arrays are considered to implement the interface {@code Cloneable} and that the return type
-     * of the {@code clone} method of an array type {@code T[]} is {@code T[]} where T is any reference or primitive
-     * type. Otherwise, this method creates a new instance of the class of this object and initializes all its fields
-     * with exactly the contents of the corresponding fields of this object, as if by assignment; the contents of the
-     * fields are not themselves cloned. Thus, this method performs a "shallow copy" of this object, not a "deep copy"
-     * operation.
-     * <p>
-     * The class {@code Object} does not itself implement the interface {@code Cloneable}, so calling the {@code clone}
-     * method on an object whose class is {@code Object} will result in throwing an exception at run time.
-     *
-     * @return a clone of this instance.
-     * @throws CloneNotSupportedException if the object's class does not support the {@code Cloneable} interface.
-     *                                    Subclasses that override the {@code clone} method can also throw this
-     *                                    exception to indicate that an instance cannot be cloned.
-     * @see Cloneable
-     */
-    @Override
-    protected Object clone() throws CloneNotSupportedException {
-        return super.clone();
     }
 
     /**
@@ -676,6 +414,6 @@ public class SmileyVarsJdbcTemplate extends JdbcTemplate {
      */
     @Override
     public String toString() {
-        return super.toString();
+        return "SmileyVarsJdbcTemplate[databaseType: " + dbType + "; " + super.toString() + "]";
     }
 }
