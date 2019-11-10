fccdcfa244160a3e0c1e6b97396350e1cd8c3e37
Mark Grand <mgrand@markgrand.com>
2019-11-10 18:33:32 -0500

    code complete

--- smiley-vars/src/main/java/com/markgrand/smileyVars/NullValue.java
+++ smiley-vars/src/main/java/com/markgrand/smileyVars/NullValue.java
@@ -1,10 +1,33 @@
 package com.markgrand.smileyVars;
 
 /**
- * Singleton class used to represent a null value for variables that have a null value.
+ * Singleton class used to represent a null value for variables that have a null value. This is useful for setting
+ * parameters of PreparedStatement and CallableStatement objects, because we need to know the type of null value.
  */
 class NullValue {
-    static final NullValue INSTANCE = new NullValue();
+    private int type;
 
-    private NullValue() {}
+    /**
+     * Constructor
+     *
+     * @param type A type constant from {@link java.sql.Types}.
+     */
+    NullValue(int type) {
+        this.type = type;
+    }
+
+    /**
+     * Return the type associated with this object, which should be one of the values defined in {@link
+     * java.sql.Types}.
+     *
+     * @return
+     */
+    int getType() {
+        return type;
+    }
+
+    @Override
+    public String toString() {
+        return "null";
+    }
 }
