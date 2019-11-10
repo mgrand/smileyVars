127f74c9232618cb7e81c9bacd2cb660d42a88ca
Mark Grand <mgrand@markgrand.com>
2019-11-10 16:00:20 -0500

    Added content.

--- smiley-vars/src/main/java/com/markgrand/smileyVars/package-info.java
+++ smiley-vars/src/main/java/com/markgrand/smileyVars/package-info.java
@@ -1,5 +1,7 @@
 /**
  * This is the main package for SmileyVars.
+ *
+ * <p>The main class for using SmileyVars directly is {@link com.markgrand.smileyVars.SmileyVarsTemplate}</p>
  */
 
 package com.markgrand.smileyVars;
