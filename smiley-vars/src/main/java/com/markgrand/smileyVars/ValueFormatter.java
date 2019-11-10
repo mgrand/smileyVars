68a85468122a0598157749bcdb7cdfe2c35a4bca
Mark Grand <mgrand@markgrand.com>
2019-11-09 20:22:56 -0500

    Added isDefault

--- smiley-vars/src/main/java/com/markgrand/smileyVars/ValueFormatter.java
+++ smiley-vars/src/main/java/com/markgrand/smileyVars/ValueFormatter.java
@@ -13,6 +13,7 @@ import java.util.function.Predicate;
  * instance of a class that is not a number, string or date.
  */
 class ValueFormatter {
+    private final Predicate<Object> isDefaultFor;
     private final Predicate<Object> appliesTo;
     private final Function<Object, String> formattingFunction;
     private final PreparedStatementSetter preparedStatementSetter;
@@ -21,26 +22,38 @@ class ValueFormatter {
     /**
      * Constructor
      *
-     * @param predicate          This should return true if, and only if, its argument is something that the
+     * @param isDefaultFor       Predicate that returns true if this formatter is the default formatter for the given
+     *                           type of object.
+     * @param appliesTo          Predicate that returns true if, and only if, its argument is something that the
      *                           formattingFunction can format.
-     * @param formattingFunction If given a value that the predicate returns true for, this should return a string that
+     * @param formattingFunction If given a value that appliesTo returns true for, this should return a string that
      *                           represents to given value as an SQL literal.
      * @param name               The name of this formatter.
      */
-    ValueFormatter(Predicate<Object> predicate,
+    ValueFormatter(Predicate<Object> isDefaultFor,
+                   Predicate<Object> appliesTo,
                    Function<Object, String> formattingFunction,
                    PreparedStatementSetter preparedStatementSetter,
                    String name) {
-        appliesTo = predicate;
+        this.isDefaultFor = isDefaultFor;
+        this.appliesTo = appliesTo;
         this.formattingFunction = formattingFunction;
         this.preparedStatementSetter = preparedStatementSetter;
         this.name = name;
     }
 
     /**
-     * Return {@code true} if the this object can formatting the given value, otherwise {@code false}.
+     * Return {@code true} if this object can format the given value, otherwise {@code false}.
      *
-     * @param value an object to be formatted as an SQL literal.
+     * @param value an object to be formatted.
+     * @return {@code true} if this object can format the given value, otherwise {@code false}.
+     */
+    boolean isDefaultFor(Object value) { return isDefaultFor.test(value); }
+
+    /**
+     * Return {@code true} if this object can format the given value, otherwise {@code false}.
+     *
+     * @param value an object to be formatted.
      * @return {@code true} if the this object can formatting the given value, otherwise {@code false}.
      */
     boolean isApplicable(Object value) {
