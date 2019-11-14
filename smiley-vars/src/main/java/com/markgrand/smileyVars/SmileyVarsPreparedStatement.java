2fa85736fe00c3eaf2cbb4132533942439821ae6
Mark Grand <mgrand@markgrand.com>
2019-11-14 05:34:30 -0500

    added support for float

--- smiley-vars/src/main/java/com/markgrand/smileyVars/SmileyVarsPreparedStatement.java
+++ smiley-vars/src/main/java/com/markgrand/smileyVars/SmileyVarsPreparedStatement.java
@@ -162,14 +162,12 @@ public class SmileyVarsPreparedStatement {
      * Sets the designated parameter to the given Java <code>float</code> value. The driver converts this to an SQL
      * <code>REAL</code> value when it sends it to the database.
      *
-     * @param parameterIndex the first parameter is 1, the second is 2, ...
-     * @param x              the parameter value
-     * @throws SQLException if parameterIndex does not correspond to a parameter marker in the SQL statement; if a
-     *                      database access error occurs or this method is called on a closed
-     *                      <code>PreparedStatement</code>
+     * @param parameterName The name of the parameter.
+     * @param value         the parameter value
+     * @throws SQLException If parameterName does not correspond to a variable in the SmilelyVars template.
      */
-    public void setFloat(int parameterIndex, float x) throws SQLException {
-        //TODO finish this
+    public void setFloat(String parameterName, float value) throws SQLException {
+        changeWithCheckedName(parameterName, value, (name, val) -> valueMap.put(name, new FloatValue(val)));
     }
 
     /**
