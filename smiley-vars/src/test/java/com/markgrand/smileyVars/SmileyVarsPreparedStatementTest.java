f3352af2fcd43b87d50a41305ef26c4a42045208
Mark Grand <mgrand@markgrand.com>
2019-11-24 20:38:41 -0500

    New File

---
+++ smiley-vars/src/test/java/com/markgrand/smileyVars/SmileyVarsPreparedStatementTest.java
@@ -0,0 +1,493 @@
+package com.markgrand.smileyVars;
+
+import jdk.nashorn.internal.ir.annotations.Ignore;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SmileyVarsPreparedStatementTest {
+    @Ignore
+    @Test
+    void executeQuery() {
+    }
+
+    @Ignore
+    @Test
+    void executeUpdate() {
+    }
+
+    @Ignore
+    @Test
+    void setNull() {
+    }
+
+    @Ignore
+    @Test
+    void setBoolean() {
+    }
+
+    @Ignore
+    @Test
+    void setByte() {
+    }
+
+    @Ignore
+    @Test
+    void setShort() {
+    }
+
+    @Ignore
+    @Test
+    void setInt() {
+    }
+
+    @Ignore
+    @Test
+    void setLong() {
+    }
+
+    @Ignore
+    @Test
+    void setFloat() {
+    }
+
+    @Ignore
+    @Test
+    void setDouble() {
+    }
+
+    @Ignore
+    @Test
+    void setBigDecimal() {
+    }
+
+    @Ignore
+    @Test
+    void setString() {
+    }
+
+    @Ignore
+    @Test
+    void setBytes() {
+    }
+
+    @Ignore
+    @Test
+    void setDate() {
+    }
+
+    @Ignore
+    @Test
+    void setTime() {
+    }
+
+    @Ignore
+    @Test
+    void setTimestamp() {
+    }
+
+    @Ignore
+    @Test
+    void setAsciiStream() {
+    }
+
+    @Ignore
+    @Test
+    void setBinaryStream() {
+    }
+
+    @Ignore
+    @Test
+    void setObject() {
+    }
+
+    @Ignore
+    @Test
+    void testSetObject() {
+    }
+
+    @Ignore
+    @Test
+    void execute() {
+    }
+
+    @Ignore
+    @Test
+    void setCharacterStream() {
+    }
+
+    @Ignore
+    @Test
+    void setRef() {
+    }
+
+    @Ignore
+    @Test
+    void setBlob() {
+    }
+
+    @Ignore
+    @Test
+    void setClob() {
+    }
+
+    @Test
+    @Ignore
+    void getMetaData() {
+    }
+
+    @Ignore
+    @Test
+    void setArray() {
+    }
+
+    @Ignore
+    @Test
+    void testSetDate() {
+    }
+
+    @Ignore
+    @Test
+    void testSetTime() {
+    }
+
+    @Ignore
+    @Test
+    void testSetTimestamp() {
+    }
+
+    @Ignore
+    @Test
+    void testSetNull() {
+    }
+
+    @Ignore
+    @Test
+    void setURL() {
+    }
+
+    @Ignore
+    @Test
+    void getParameterMetaData() {
+    }
+
+    @Ignore
+    @Test
+    void setRowId() {
+    }
+
+    @Ignore
+    @Test
+    void setNString() {
+    }
+
+    @Ignore
+    @Test
+    void setNCharacterStream() {
+    }
+
+    @Ignore
+    @Test
+    void setNClob() {
+    }
+
+    @Ignore
+    @Test
+    void testSetClob() {
+    }
+
+    @Ignore
+    @Test
+    void testSetBlob() {
+    }
+
+    @Ignore
+    @Test
+    void testSetNClob() {
+    }
+
+    @Ignore
+    @Test
+    void setSQLXML() {
+    }
+
+    @Ignore
+    @Test
+    void testSetObject1() {
+    }
+
+    @Ignore
+    @Test
+    void testSetAsciiStream() {
+    }
+
+    @Ignore
+    @Test
+    void testSetBinaryStream() {
+    }
+
+    @Ignore
+    @Test
+    void testSetCharacterStream() {
+    }
+
+    @Ignore
+    @Test
+    void testSetAsciiStream1() {
+    }
+
+    @Ignore
+    @Test
+    void testSetBinaryStream1() {
+    }
+
+    @Ignore
+    @Test
+    void testSetCharacterStream1() {
+    }
+
+    @Ignore
+    @Test
+    void testSetNCharacterStream() {
+    }
+
+    @Ignore
+    @Test
+    void testSetClob1() {
+    }
+
+    @Ignore
+    @Test
+    void testSetBlob1() {
+    }
+
+    @Ignore
+    @Test
+    void testSetNClob1() {
+    }
+
+    @Ignore
+    @Test
+    void testExecuteQuery() {
+    }
+
+    @Ignore
+    @Test
+    void testExecuteUpdate() {
+    }
+
+    @Ignore
+    @Test
+    void close() {
+    }
+
+    @Ignore
+    @Test
+    void clearParameters() {
+    }
+
+    @Ignore
+    @Test
+    void deepClearParameters() {
+    }
+
+    @Ignore
+    @Test
+    void getMaxFieldSize() {
+    }
+
+    @Ignore
+    @Test
+    void setMaxFieldSize() {
+    }
+
+    @Ignore
+    @Test
+    void getMaxRows() {
+    }
+
+    @Ignore
+    @Test
+    void setMaxRows() {
+    }
+
+    @Ignore
+    @Test
+    void getQueryTimeout() {
+    }
+
+    @Ignore
+    @Test
+    void setQueryTimeout() {
+    }
+
+    @Ignore
+    @Test
+    void getWarnings() {
+    }
+
+    @Ignore
+    @Test
+    void clearWarnings() {
+    }
+
+    @Ignore
+    @Test
+    void setCursorName() {
+    }
+
+    @Ignore
+    @Test
+    void testExecute() {
+    }
+
+    @Ignore
+    @Test
+    void getResultSet() {
+    }
+
+    @Ignore
+    @Test
+    void getUpdateCount() {
+    }
+
+    @Ignore
+    @Test
+    void getMoreResults() {
+    }
+
+    @Ignore
+    @Test
+    void getFetchDirection() {
+    }
+
+    @Ignore
+    @Test
+    void setFetchDirection() {
+    }
+
+    @Ignore
+    @Test
+    void getFetchSize() {
+    }
+
+    @Ignore
+    @Test
+    void setFetchSize() {
+    }
+
+    @Ignore
+    @Test
+    void getResultSetConcurrency() {
+    }
+
+    @Ignore
+    @Test
+    void getResultSetType() {
+    }
+
+    @Ignore
+    @Test
+    void getConnection() {
+    }
+
+    @Ignore
+    @Test
+    void testGetMoreResults() {
+    }
+
+    @Ignore
+    @Test
+    void getGeneratedKeys() {
+    }
+
+    @Ignore
+    @Test
+    void testExecuteUpdate1() {
+    }
+
+    @Ignore
+    @Test
+    void testExecuteUpdate2() {
+    }
+
+    @Ignore
+    @Test
+    void testExecuteUpdate3() {
+    }
+
+    @Ignore
+    @Test
+    void testExecute1() {
+    }
+
+    @Ignore
+    @Test
+    void testExecute2() {
+    }
+
+    @Ignore
+    @Test
+    void testExecute3() {
+    }
+
+    @Ignore
+    @Test
+    void getResultSetHoldability() {
+    }
+
+    @Ignore
+    @Test
+    void isClosed() {
+    }
+
+    @Ignore
+    @Test
+    void isPoolable() {
+    }
+
+    @Ignore
+    @Test
+    void setPoolable() {
+    }
+
+    @Ignore
+    @Test
+    void getLargeUpdateCount() {
+    }
+
+    @Ignore
+    @Test
+    void getLargeMaxRows() {
+    }
+
+    @Ignore
+    @Test
+    void setLargeMaxRows() {
+    }
+
+    @Ignore
+    @Test
+    void executeLargeUpdate() {
+    }
+
+    @Ignore
+    @Test
+    void testExecuteLargeUpdate() {
+    }
+
+    @Ignore
+    @Test
+    void testExecuteLargeUpdate1() {
+    }
+
+    @Ignore
+    @Test
+    void testExecuteLargeUpdate2() {
+    }
+
+    @Ignore
+    @Test
+    void getPreparedStatement() {
+    }
+}
