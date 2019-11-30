e03c18d13146ad2ab412e593a832279cc1044e46
Mark Grand <mgrand@markgrand.com>
2019-11-30 18:46:52 -0500

    optimized imports

--- smiley-vars/src/test/java/com/markgrand/smileyVars/SmileyVarsPreparedStatementTest.java
+++ smiley-vars/src/test/java/com/markgrand/smileyVars/SmileyVarsPreparedStatementTest.java
@@ -1,6 +1,5 @@
 package com.markgrand.smileyVars;
 
-import jdk.nashorn.internal.ir.annotations.Ignore;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
