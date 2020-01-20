956262e8d0cba996ce523a99418edef8dcda44c0
Mark Grand <mgrand@markgrand.com>
2020-01-19 20:57:50 -0500

    Added builder to MapSetter

--- smiley-vars/src/main/java/com/markgrand/smileyvars/MapSetter.java
+++ smiley-vars/src/main/java/com/markgrand/smileyvars/MapSetter.java
@@ -3,7 +3,12 @@ package com.markgrand.smileyvars;
 import com.markgrand.smileyvars.util.BiSqlConsumer;
 import org.jetbrains.annotations.NotNull;
 
-import java.sql.SQLException;
+import java.math.BigDecimal;
+import java.net.URL;
+import java.sql.*;
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -39,27 +44,334 @@ public class MapSetter {
         this.setterMap = setterMap;
     }
 
+    public static Builder newBuilder() {
+        return new Builder();
+    }
+
     /**
      * Set smileyVars of the given {@link SmileyVarsPreparedStatement} from the values in the valueMap. Any previously
      * set smileyVars are first cleared.
      *
-     * @param svps the {@link SmileyVarsPreparedStatement} whose parameters are to be set.
+     * @param svps     the {@link SmileyVarsPreparedStatement} whose parameters are to be set.
      * @param valueMap a Map containing the names and values to be set.
      * @return the {@link SmileyVarsPreparedStatement}
-     * @throws SQLException if any of the setters throw an SQLException
+     * @throws SQLException        if any of the setters throw an SQLException
      * @throws SmileyVarsException If there is a problem setting the values of the smileyVars.
      */
+    @SuppressWarnings("UnusedReturnValue")
     public SmileyVarsPreparedStatement setSmileyVars(@NotNull SmileyVarsPreparedStatement svps, Map<String, Object> valueMap) throws SQLException {
         svps.clearParameters();
-        for (Map.Entry<String, Object> valueEntry: valueMap.entrySet()) {
-            setterMap.computeIfAbsent(valueEntry.getKey(), nm->throwException(nm)).accept(svps, valueEntry.getValue());
+        for (Map.Entry<String, Object> valueEntry : valueMap.entrySet()) {
+            setterMap.computeIfAbsent(valueEntry.getKey(), this::throwException).accept(svps, valueEntry.getValue());
         }
         return svps;
     }
 
     private BiSqlConsumer<SmileyVarsPreparedStatement, Object> throwException(String name) {
         String msg = "MapSetter was not constructed with a setter for a SmileyVar named " + name
-                + "; The names that it was constructed with are " + setterMap.keySet();
+                             + "; The names that it was constructed with are " + setterMap.keySet();
         throw new SmileyVarsException(msg);
+    }
+
+    /**
+     * Return a read-only view of the map that specifies the method to call for each simpleVar name.
+     *
+     * @return the read-only view.
+     */
+    public Map<String, BiSqlConsumer<SmileyVarsPreparedStatement, Object>> getSetterMap() {
+        return Collections.unmodifiableMap(setterMap);
+    }
+
+    /**
+     * Builder class for creating {@link MapSetter} objects.
+     */
+    public static class Builder {
+        private Map<String, BiSqlConsumer<SmileyVarsPreparedStatement, Object>> setterMap = new HashMap<>();
+
+        private Builder() {
+        }
+
+        /**
+         * Configure the {@link MapSetter} to use {@code setBoolean} to set the smileyVar with the specified name.
+         *
+         * @param name The name of the smileyVar to be configured.
+         * @return this object.
+         */
+        Builder booleanVar(@SuppressWarnings("SameParameterValue") String name) {
+            setterMap.put(name, (svps, value) -> svps.setBoolean(name, (boolean) value));
+            return this;
+        }
+
+        /**
+         * Configure the {@link MapSetter} to use {@code setByte} to set the smileyVar with the specified name.
+         *
+         * @param name The name of the smileyVar to be configured.
+         * @return this object.
+         */
+        @SuppressWarnings("SameParameterValue")
+        Builder byteVar(String name) {
+            setterMap.put(name, (svps, value) -> svps.setByte(name, (byte) value));
+            return this;
+        }
+
+        /**
+         * Configure the {@link MapSetter} to use {@code setShort} to set the smileyVar with the specified name.
+         *
+         * @param name The name of the smileyVar to be configured.
+         * @return this object.
+         */
+        @SuppressWarnings("SameParameterValue")
+        Builder shortVar(String name) {
+            setterMap.put(name, (svps, value) -> svps.setShort(name, (short) value));
+            return this;
+        }
+
+        /**
+         * Configure the {@link MapSetter} to use {@code setInt} to set the smileyVar with the specified name.
+         *
+         * @param name The name of the smileyVar to be configured.
+         * @return this object.
+         */
+        @SuppressWarnings("SameParameterValue")
+        Builder intVar(String name) {
+            setterMap.put(name, (svps, value) -> svps.setInt(name, (int) value));
+            return this;
+        }
+
+        /**
+         * Configure the {@link MapSetter} to use {@code setLong} to set the smileyVar with the specified name.
+         *
+         * @param name The name of the smileyVar to be configured.
+         * @return this object.
+         */
+        @SuppressWarnings("SameParameterValue")
+        Builder longVar(String name) {
+            setterMap.put(name, (svps, value) -> svps.setLong(name, (long) value));
+            return this;
+        }
+
+        /**
+         * Configure the {@link MapSetter} to use {@code setFloat} to set the smileyVar with the specified name.
+         *
+         * @param name The name of the smileyVar to be configured.
+         * @return this object.
+         */
+        @SuppressWarnings("SameParameterValue")
+        Builder floatVar(String name) {
+            setterMap.put(name, (svps, value) -> svps.setFloat(name, (float) value));
+            return this;
+        }
+
+        /**
+         * Configure the {@link MapSetter} to use {@code setDouble} to set the smileyVar with the specified name.
+         *
+         * @param name The name of the smileyVar to be configured.
+         * @return this object.
+         */
+        @SuppressWarnings("SameParameterValue")
+        Builder doubleVar(String name) {
+            setterMap.put(name, (svps, value) -> svps.setDouble(name, (double) value));
+            return this;
+        }
+
+        /**
+         * Configure the {@link MapSetter} to use {@code setBigDecimal} to set the smileyVar with the specified name.
+         *
+         * @param name The name of the smileyVar to be configured.
+         * @return this object.
+         */
+        @SuppressWarnings("SameParameterValue")
+        Builder bigDecimalVar(String name) {
+            setterMap.put(name, (svps, value) -> svps.setBigDecimal(name, (BigDecimal) value));
+            return this;
+        }
+
+        /**
+         * Configure the {@link MapSetter} to use {@code setString} to set the smileyVar with the specified name.
+         *
+         * @param name The name of the smileyVar to be configured.
+         * @return this object.
+         */
+        @SuppressWarnings("SameParameterValue")
+        Builder stringVar(String name) {
+            setterMap.put(name, (svps, value) -> svps.setString(name, (String) value));
+            return this;
+        }
+
+        /**
+         * Configure the {@link MapSetter} to use {@code setBytes} to set the smileyVar with the specified name.
+         *
+         * @param name The name of the smileyVar to be configured.
+         * @return this object.
+         */
+        @SuppressWarnings("SameParameterValue")
+        Builder bytesVar(String name) {
+            setterMap.put(name, (svps, value) -> svps.setBytes(name, (byte[]) value));
+            return this;
+        }
+
+        /**
+         * Configure the {@link MapSetter} to use {@code setDate} to set the smileyVar with the specified name.
+         *
+         * @param name The name of the smileyVar to be configured.
+         * @return this object.
+         */
+        @SuppressWarnings("SameParameterValue")
+        Builder dateVar(String name) {
+            setterMap.put(name, (svps, value) -> svps.setDate(name, (Date) value));
+            return this;
+        }
+
+        /**
+         * Configure the {@link MapSetter} to use {@code setTime} to set the smileyVar with the specified name.
+         *
+         * @param name The name of the smileyVar to be configured.
+         * @return this object.
+         */
+        @SuppressWarnings("SameParameterValue")
+        Builder timeVar(String name) {
+            setterMap.put(name, (svps, value) -> svps.setTime(name, (Time) value));
+            return this;
+        }
+
+        /**
+         * Configure the {@link MapSetter} to use {@code setTimestamp} to set the smileyVar with the specified name.
+         *
+         * @param name The name of the smileyVar to be configured.
+         * @return this object.
+         */
+        @SuppressWarnings("SameParameterValue")
+        Builder timestampVar(String name) {
+            setterMap.put(name, (svps, value) -> svps.setTimestamp(name, (Timestamp) value));
+            return this;
+        }
+
+        /**
+         * Configure the {@link MapSetter} to use {@code setObject} to set the smileyVar with the specified name.
+         *
+         * @param name          The name of the smileyVar to be configured.
+         * @param targetSqlType the SQL type (as defined in java.sql.Types) to be sent to the database
+         * @return this object.
+         */
+        @SuppressWarnings("SameParameterValue")
+        Builder objectVar(String name, int targetSqlType) {
+            setterMap.put(name, (svps, value) -> svps.setObject(name, value, targetSqlType));
+            return this;
+        }
+
+        /**
+         * Configure the {@link MapSetter} to use {@code setBlob} to set the smileyVar with the specified name.
+         *
+         * @param name The name of the smileyVar to be configured.
+         * @return this object.
+         */
+        @SuppressWarnings("SameParameterValue")
+        Builder blobVar(String name) {
+            setterMap.put(name, (svps, value) -> svps.setBlob(name, (Blob) value));
+            return this;
+        }
+
+        /**
+         * Configure the {@link MapSetter} to use {@code setClob} to set the smileyVar with the specified name.
+         *
+         * @param name The name of the smileyVar to be configured.
+         * @return this object.
+         */
+        @SuppressWarnings("SameParameterValue")
+        Builder clobVar(String name) {
+            setterMap.put(name, (svps, value) -> svps.setClob(name, (Clob) value));
+            return this;
+        }
+
+        /**
+         * Configure the {@link MapSetter} to use {@code setArray} to set the smileyVar with the specified name.
+         *
+         * @param name The name of the smileyVar to be configured.
+         * @return this object.
+         */
+        @SuppressWarnings("SameParameterValue")
+        Builder arrayVar(String name) {
+            setterMap.put(name, (svps, value) -> svps.setArray(name, (Array) value));
+            return this;
+        }
+
+        /**
+         * Configure the {@link MapSetter} to use {@code setDate} to set the smileyVar with the specified name.
+         *
+         * @param name     The name of the smileyVar to be configured.
+         * @param calendar the <code>Calendar</code> object the driver will use to construct the date
+         * @return this object.
+         */
+        @SuppressWarnings("SameParameterValue")
+        Builder dateVar(String name, Calendar calendar) {
+            setterMap.put(name, (svps, value) -> svps.setDate(name, (Date) value, calendar));
+            return this;
+        }
+
+        /**
+         * Configure the {@link MapSetter} to use {@code setTime} to set the smileyVar with the specified name.
+         *
+         * @param name     The name of the smileyVar to be configured.
+         * @param calendar the <code>Calendar</code> object the driver will use to construct the date
+         * @return this object.
+         */
+        @SuppressWarnings("SameParameterValue")
+        Builder timeVar(String name, Calendar calendar) {
+            setterMap.put(name, (svps, value) -> svps.setTime(name, (Time) value, calendar));
+            return this;
+        }
+
+        /**
+         * Configure the {@link MapSetter} to use {@code setTimestamp} to set the smileyVar with the specified name.
+         *
+         * @param name     The name of the smileyVar to be configured.
+         * @param calendar the <code>Calendar</code> object the driver will use to construct the date
+         * @return this object.
+         */
+        @SuppressWarnings("SameParameterValue")
+        Builder timestampVar(String name, Calendar calendar) {
+            setterMap.put(name, (svps, value) -> svps.setTimestamp(name, (Timestamp) value, calendar));
+            return this;
+        }
+
+        /**
+         * Configure the {@link MapSetter} to use {@code setURL} to set the smileyVar with the specified name.
+         *
+         * @param name The name of the smileyVar to be configured.
+         * @return this object.
+         */
+        @SuppressWarnings("SameParameterValue")
+        Builder urlVar(String name) {
+            setterMap.put(name, (svps, value) -> svps.setURL(name, (URL) value));
+            return this;
+        }
+
+        /**
+         * Configure the {@link MapSetter} to use {@code setNString} to set the smileyVar with the specified name.
+         *
+         * @param name The name of the smileyVar to be configured.
+         * @return this object.
+         */
+        @SuppressWarnings("SameParameterValue")
+        Builder nStringVar(String name) {
+            setterMap.put(name, (svps, value) -> svps.setNString(name, (String) value));
+            return this;
+        }
+
+        /**
+         * Configure the {@link MapSetter} to use {@code setNClob} to set the smileyVar with the specified name.
+         *
+         * @param name The name of the smileyVar to be configured.
+         * @return this object.
+         */
+        @SuppressWarnings("SameParameterValue")
+        Builder nClob(String name) {
+            setterMap.put(name, (svps, value) -> svps.setNClob(name, (NClob) value));
+            return this;
+        }
+
+        MapSetter build() {
+            return new MapSetter(setterMap);
+        }
     }
 }
--- smiley-vars/src/test/java/com/markgrand/smileyvars/MapSetterTest.java
+++ smiley-vars/src/test/java/com/markgrand/smileyvars/MapSetterTest.java
@@ -1,13 +1,26 @@
 package com.markgrand.smileyvars;
 
 import com.markgrand.smileyvars.util.BiSqlConsumer;
+import com.mockrunner.mock.jdbc.MockArray;
+import com.mockrunner.mock.jdbc.MockBlob;
+import com.mockrunner.mock.jdbc.MockClob;
+import com.mockrunner.mock.jdbc.MockNClob;
+import mockit.Expectations;
 import mockit.Mocked;
 import mockit.Verifications;
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
+import java.net.URL;
+import java.sql.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class MapSetterTest {
@@ -15,8 +28,8 @@ class MapSetterTest {
     @Test
     void setSmileyVars(@Mocked SmileyVarsPreparedStatement svps) throws Exception {
         Map<String, BiSqlConsumer<SmileyVarsPreparedStatement, Object>> setterMap = new HashMap<>();
-        setterMap.put("quantity", (ps, value)->ps.setInt("quantity", (int)value));
-        setterMap.put("description", (ps, value)->ps.setString("description", (String)value));
+        setterMap.put("quantity", (ps, value) -> ps.setInt("quantity", (int) value));
+        setterMap.put("description", (ps, value) -> ps.setString("description", (String) value));
         MapSetter mapSetter = new MapSetter(setterMap);
 
         Map<String, Object> valueMap = new HashMap<>();
@@ -24,22 +37,125 @@ class MapSetterTest {
         valueMap.put("description", "foobar");
         mapSetter.setSmileyVars(svps, valueMap);
         new Verifications() {{
-           svps.clearParameters(); times = 1;
-           svps.setInt("quantity", 4); times = 1;
-           svps.setString("description", "foobar"); times = 1;
+            svps.clearParameters();
+            times = 1;
+            svps.setInt("quantity", 4);
+            times = 1;
+            svps.setString("description", "foobar");
+            times = 1;
         }};
+    }
+
+    @Test
+    void getSetterMap() {
+        Map<String, BiSqlConsumer<SmileyVarsPreparedStatement, Object>> setterMap = new HashMap<>();
+        setterMap.put("quantity", (ps, value) -> ps.setInt("quantity", (int) value));
+        setterMap.put("description", (ps, value) -> ps.setString("description", (String) value));
+        MapSetter mapSetter = new MapSetter(setterMap);
+        assertEquals(setterMap, mapSetter.getSetterMap());
     }
 
     @Test
     void setSmileyVarsWrongName(@Mocked SmileyVarsPreparedStatement svps) {
         Map<String, BiSqlConsumer<SmileyVarsPreparedStatement, Object>> setterMap = new HashMap<>();
-        setterMap.put("quantity", (ps, value)->ps.setInt("quantity", (int)value));
-        setterMap.put("description", (ps, value)->ps.setString("description", (String)value));
+        setterMap.put("quantity", (ps, value) -> ps.setInt("quantity", (int) value));
+        setterMap.put("description", (ps, value) -> ps.setString("description", (String) value));
         MapSetter mapSetter = new MapSetter(setterMap);
 
         Map<String, Object> valueMap = new HashMap<>();
         valueMap.put("qty", 4);
         valueMap.put("description", "foobar");
-        assertThrows(SmileyVarsException.class, ()->mapSetter.setSmileyVars(svps, valueMap));
+        assertThrows(SmileyVarsException.class, () -> mapSetter.setSmileyVars(svps, valueMap));
+    }
+
+    private Array mockArray = new MockArray(new int[]{0, 1, 2, 3});
+    private Blob mockBlob = new MockBlob("Bogus".getBytes());
+    private Calendar calendar = Calendar.getInstance();
+    private Clob mockClob = new MockClob("eieio");
+    private Date date = Date.valueOf(LocalDate.now());
+    private NClob mockNClob = new MockNClob("Banjo");
+    private Time time = Time.valueOf(LocalTime.now());
+    private Timestamp timestamp = Timestamp.valueOf(LocalDateTime.now());
+
+    @Test
+    void builder(@Mocked SmileyVarsPreparedStatement svps) throws Exception {
+        MapSetter mapSetter = MapSetter.newBuilder()
+                                      .arrayVar("array")
+                                      .bigDecimalVar("bigDecimal")
+                                      .blobVar("blobvar")
+                                      .booleanVar("boolean")
+                                      .bytesVar("bytes")
+                                      .byteVar("byte")
+                                      .bytesVar("bytes")
+                                      .clobVar("clob")
+                                      .dateVar("date")
+                                      .dateVar("calendarDate", calendar)
+                                      .doubleVar("double")
+                                      .floatVar("float")
+                                      .intVar("int")
+                                      .longVar("long")
+                                      .nClob("nClob")
+                                      .nStringVar("nString")
+                                      .objectVar("object", Types.DECIMAL)
+                                      .shortVar("short")
+                                      .stringVar("string")
+                                      .timestampVar("timestamp")
+                                      .timestampVar("calendarTimestamp", calendar)
+                                      .timeVar("time")
+                                      .timeVar("calendarTime", calendar)
+                                      .urlVar("url")
+                                      .build();
+        Map<String, Object> valueMap = new HashMap<>();
+        valueMap.put("array", mockArray);
+        valueMap.put("bigDecimal", BigDecimal.valueOf(123));
+        valueMap.put("blobvar", mockBlob);
+        valueMap.put("boolean", true);
+        valueMap.put("bytes", "BoGuS".getBytes());
+        valueMap.put("byte", (byte)23);
+        valueMap.put("clob", mockClob);
+        valueMap.put("date", date);
+        valueMap.put("calendarDate", date);
+        valueMap.put("double", 123456.0625D);
+        valueMap.put("float", 123.456F);
+        valueMap.put("int", 19238);
+        valueMap.put("long", 987654321L);
+        valueMap.put("nClob", mockNClob);
+        valueMap.put("nString", "nnnnnn");
+        valueMap.put("object", 123456);
+        valueMap.put("short", (short)12345);
+        valueMap.put("string", "foobar");
+        valueMap.put("timestamp", timestamp);
+        valueMap.put("calendarTimestamp", timestamp);
+        valueMap.put("time", time);
+        valueMap.put("calendarTime", time);
+        valueMap.put("url", new URL("http://example.com"));
+
+        new Expectations() {{
+            svps.setArray("array", mockArray);
+            svps.setBigDecimal("bigDecimal", BigDecimal.valueOf(123));
+            svps.setBlob("blobvar", mockBlob);
+            svps.setBoolean("boolean", true);
+            svps.setBytes("bytes", "BoGuS".getBytes());
+            svps.setByte("byte", (byte)23);
+            svps.setClob("clob", mockClob);
+            svps.setDate("date", date);
+            svps.setDate("calendarDate", date, calendar);
+            svps.setDouble("double", 123456.0625D);
+            svps.setFloat("float", 123.456F);
+            svps.setInt("int", 19238);
+            svps.setLong("long", 987654321L);
+            svps.setNClob("nClob", mockNClob);
+            svps.setNString("nString", "nnnnnn");
+            svps.setObject("object", 123456, Types.DECIMAL);
+            svps.setShort("short", (short)12345);
+            svps.setString("string", "foobar");
+            svps.setTimestamp("timestamp", timestamp);
+            svps.setTimestamp("calendarTimestamp", timestamp, calendar);
+            svps.setTime("time", time);
+            svps.setTime("calendarTime", time, calendar);
+            svps.setURL("url", new URL("http://example.com"));
+        }};
+
+        mapSetter.setSmileyVars(svps, valueMap);
     }
 }
